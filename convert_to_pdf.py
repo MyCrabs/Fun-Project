b4d2414565be5b1bda31c790bb2bb8a107898b22
Your Name <you@example.com>
2024-04-15 22:06:22 +0700

    first init

---
+++ convert_to_pdf.py
@@ -0,0 +1,2 @@
+from docx2pdf import convert
+convert(r"C:\Users\ADMIN\Desktop\Crawl_PBL4.docx", r"C:\Users\ADMIN\Desktop\Crawl_PBL4.pdf")
---
+++ youtube.py
@@ -0,0 +1,15 @@
+from  pytube import YouTube
+from sys import argv
+
+"""Get Info Video"""
+#link = argv[1]
+link = input("Link video: ")
+yt = YouTube(link)  
+print(f"Title: {yt.title}")
+print(f"Views: {yt.views}")
+print(f"Author: {yt.author}")
+print(f"Channel: {yt.channel_url}")
+
+"""DownLoad Video"""
+# yd = yt.streams.get_highest_resolution()
+# yd.download(r"C:\Users\ADMIN\Videos")
